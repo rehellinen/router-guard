6a97112c966f6f32d64876abb981cf6650af240f
rehellinen <rehellinen@foxmail.com>
2020-04-02 09:40:28 +0800

    feat: 路由守卫配置文件

---
+++ src/config.js
@@ -0,0 +1,28 @@
+// 不用权限验证的路由
+export const publicRoutes = [
+  '/', '/not_login'
+]
+
+/**
+ *
+ * @param path route路径
+ * @param auth 权限名称
+ */
+export const routerConfig = [
+  {
+    path: '/',
+    auth: ['auth_a', 'auth_b', 'auth_c', 'auth_d']
+  }, {
+    path: '/path_a',
+    auth: ['auth_a']
+  }, {
+    path: '/path_b',
+    auth: ['auth_b']
+  }, {
+    path: '/path_c',
+    auth: ['auth_c']
+  }, {
+    path: '/path_d',
+    auth: ['auth_d']
+  }
+]
